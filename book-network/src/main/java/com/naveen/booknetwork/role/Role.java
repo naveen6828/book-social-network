568d018cc6b5de800a814f4d76a00cf8c453a38a
naveen6828 <naveenkumar.jakkula98@gmail.com>
2024-04-18 14:46:58 -0400

    Created User, Role and Token entities and repos

---
+++ book-network/src/main/java/com/naveen/booknetwork/role/Role.java
@@ -0,0 +1,41 @@
+package com.naveen.booknetwork.role;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.naveen.booknetwork.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class Role {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    @Column(unique = true)
+    private String name;
+    @ManyToMany(mappedBy = "roles")
+    @JsonIgnore
+    private List<User> users;
+
+    @CreatedDate
+    @Column(updatable = false, nullable = false)
+    private LocalDateTime createdDate;
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime lastModifiedDate;
+
+}
