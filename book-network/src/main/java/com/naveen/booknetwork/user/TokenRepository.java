783381d63dfc8b03fb29b2987686ee12d0f5fb3e
naveen6828 <naveenkumar.jakkula98@gmail.com>
2024-04-19 14:29:08 -0400

    Implemented Jwt filter, service, Security Config, Bean config and UserDetailsServiceImpl

--- book-network/src/main/java/com/naveen/booknetwork/user/TokenRepository.java
+++ book-network/src/main/java/com/naveen/booknetwork/user/TokenRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface TokenRepository extends JpaRepository {
+public interface TokenRepository extends JpaRepository<Token, Integer> {
 
     Optional<Token> findByToken(String token);
 }
