8d0297e75b4abf2a2ceec1cde66b2b2993a3405e
naveen6828 <naveenkumar.jakkula98@gmail.com>
2024-04-24 09:59:58 -0400

    Implemented Auditing using application audit aware

--- book-network/src/main/java/com/naveen/booknetwork/user/Token.java
+++ book-network/src/main/java/com/naveen/booknetwork/user/Token.java
@@ -1,13 +1,10 @@
 package com.naveen.booknetwork.user;
 
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import lombok.*;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
@@ -17,6 +14,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Entity
 public class Token {
+
     @Id
     @GeneratedValue
     private Integer id;
@@ -30,4 +28,5 @@ public class Token {
     @ManyToOne
     @JoinColumn(name = "userId", nullable = false)
     private User user;
+
 }
