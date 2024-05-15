89db37387e1d8572d1646041b95d907882e03f00
naveen6828 <naveenkumar.jakkula98@gmail.com>
2024-05-15 15:29:24 -0400

    Implemented Book Card and Rating components

--- book-network-ui/src/app/modules/book/components/book-card/book-card.component.ts
+++ book-network-ui/src/app/modules/book/components/book-card/book-card.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { BookResponse } from 'src/app/services/models';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {BookResponse} from '../../../../services/models/book-response';
 
 @Component({
   selector: 'app-book-card',
@@ -7,15 +7,64 @@ import { BookResponse } from 'src/app/services/models';
   styleUrls: ['./book-card.component.css']
 })
 export class BookCardComponent {
+  private _book: BookResponse = {};
+  private _manage = false;
+  private _bookCover: string | undefined;
 
-  private _book: BookResponse = {}
-
-  get book(): BookResponse{
-    return this._book
+  get bookCover(): string | undefined {
+    if (this._book.cover) {
+      return 'data:image/jpg;base64,' + this._book.cover
+    }
+    return 'https://source.unsplash.com/user/c_v_r/1900x800';
   }
 
-  set book(value: BookResponse){
+  get book(): BookResponse {
+    return this._book;
+  }
+
+  @Input()
+  set book(value: BookResponse) {
     this._book = value;
   }
 
+
+  get manage(): boolean {
+    return this._manage;
+  }
+
+  @Input()
+  set manage(value: boolean) {
+    this._manage = value;
+  }
+
+  @Output() private share: EventEmitter<BookResponse> = new EventEmitter<BookResponse>();
+  @Output() private archive: EventEmitter<BookResponse> = new EventEmitter<BookResponse>();
+  @Output() private addToWaitingList: EventEmitter<BookResponse> = new EventEmitter<BookResponse>();
+  @Output() private borrow: EventEmitter<BookResponse> = new EventEmitter<BookResponse>();
+  @Output() private edit: EventEmitter<BookResponse> = new EventEmitter<BookResponse>();
+  @Output() private details: EventEmitter<BookResponse> = new EventEmitter<BookResponse>();
+
+  onShare() {
+    this.share.emit(this._book);
+  }
+
+  onArchive() {
+    this.archive.emit(this._book);
+  }
+
+  onAddToWaitingList() {
+    this.addToWaitingList.emit(this._book);
+  }
+
+  onBorrow() {
+    this.borrow.emit(this._book);
+  }
+
+  onEdit() {
+    this.edit.emit(this._book);
+  }
+
+  onShowDetails() {
+    this.details.emit(this._book);
+  }
 }
--- book-network-ui/src/app/modules/book/pages/book-list/book-list.component.ts
+++ book-network-ui/src/app/modules/book/pages/book-list/book-list.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { PageResponseBookResponse } from 'src/app/services/models/page-response-book-response';
-import { BookService } from 'src/app/services/services';
+
+import {Component, OnInit} from '@angular/core';
+import {BookService} from '../../../../services/services/book.service';
+import {PageResponseBookResponse} from '../../../../services/models/page-response-book-response';
+import {BookResponse} from '../../../../services/models/book-response';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-book-list',
@@ -9,13 +11,12 @@ import { BookService } from 'src/app/services/services';
   styleUrls: ['./book-list.component.css']
 })
 export class BookListComponent implements OnInit {
-
   bookResponse: PageResponseBookResponse = {};
   page = 0;
-  size = 5;
-  // pages: any = [];
-  // message = '';
-  // level: 'success' |'error' = 'success';
+  size = 1;
+  pages: any = [];
+  message = '';
+  level: 'success' |'error' = 'success';
 
   constructor(
     private bookService: BookService,
@@ -35,11 +36,61 @@ export class BookListComponent implements OnInit {
       .subscribe({
         next: (books) => {
           this.bookResponse = books;
-          // this.pages = Array(this.bookResponse.totalPages)
-          //   .fill(0)
-          //   .map((x, i) => i);
+          this.pages = Array(this.bookResponse.totalPages)
+            .fill(0)
+            .map((x, i) => i);
         }
       });
   }
 
+  gotToPage(page: number) {
+    this.page = page;
+    this.findAllBooks();
+  }
+
+  goToFirstPage() {
+    this.page = 0;
+    this.findAllBooks();
+  }
+
+  goToPreviousPage() {
+    this.page --;
+    this.findAllBooks();
+  }
+
+  goToLastPage() {
+    this.page = this.bookResponse.totalPages as number - 1;
+    this.findAllBooks();
+  }
+
+  goToNextPage() {
+    this.page++;
+    this.findAllBooks();
+  }
+
+  get isLastPage() {
+    return this.page === this.bookResponse.totalPages as number - 1;
+  }
+
+  borrowBook(book: BookResponse) {
+    this.message = '';
+    this.level = 'success';
+    this.bookService.borrowBook({
+      'book-id': book.id as number
+    }).subscribe({
+      next: () => {
+        this.level = 'success';
+        this.message = 'Book successfully added to your list';
+      },
+      error: (err) => {
+        console.log(err);
+        this.level = 'error';
+        this.message = err.error.error;
+      }
+    });
+  }
+
+  displayBookDetails(book: BookResponse) {
+    this.router.navigate(['books', 'details', book.id]);
+  }
 }
